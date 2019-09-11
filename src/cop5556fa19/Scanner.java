3ee1ec43acdd3220cb901f1114d594bf2e32e640
Madhav Sodhani <madhav2302@gmail.com>
2019-09-11 18:25:17 -0400

    Handle other tokens which doesn't require nesting. Add TODO for nested ones

--- src/cop5556fa19/Scanner.java
+++ src/cop5556fa19/Scanner.java
@@ -1,5 +1,4 @@
 
-
 /* *
  * Developed  for the class project in COP5556 Programming Language Principles 
  * at the University of Florida, Fall 2019.
@@ -14,34 +13,104 @@
  */
 package cop5556fa19;
 
-
 import static cop5556fa19.Token.Kind.*;
-
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Optional;
+
+import cop5556fa19.Token.Kind;
 
 public class Scanner {
-	
 	Reader r;
+	private int pos = 0;
+	private int line = 0;
 
+	/**
+	 * When checking the next value, if it doesn't have any sequence then we need to
+	 * save the value of char here because we won't be able to fetch it again using
+	 * r.read()
+	 */
+	private Optional<Character> lastChar = Optional.empty();
 
 	@SuppressWarnings("serial")
-	public static class LexicalException extends Exception {	
+	public static class LexicalException extends Exception {
 		public LexicalException(String arg0) {
 			super(arg0);
 		}
 	}
-	
+
 	public Scanner(Reader r) throws IOException {
 		this.r = r;
 	}
 
-
 	public Token getNext() throws Exception {
-		    //replace this code.  Just for illustration
-		    if (r.read() == -1) { return new Token(EOF,"eof",0,0);}
-			throw new LexicalException("Useful error message");
+		int current = r.read();
+
+		if (current == -1)
+			return new Token(EOF, "eof", pos, 0);
+
+		char currentChar = (char) current;
+		switch (currentChar) {
+		case '+':
+			return new Token(Kind.OP_PLUS, "+", pos++, line);
+		case '-':
+			return new Token(Kind.OP_MINUS, "-", pos++, line);
+		case '*':
+			return new Token(Kind.OP_TIMES, "*", pos++, line);
+		case '/':
+			// TODO : Check for //
+			return new Token(Kind.OP_DIV, "/", pos++, line);
+		case '%':
+			return new Token(Kind.OP_MOD, "%", pos++, line);
+		case '^':
+			return new Token(Kind.OP_POW, "^", pos++, line);
+		case '#':
+			return new Token(Kind.OP_HASH, "#", pos++, line);
+		case '&':
+			return new Token(Kind.BIT_AMP, "&", pos++, line);
+		case '~':
+			// TODO : Check for ~=
+			return new Token(Kind.BIT_XOR, "~", pos++, line);
+		case '|':
+			return new Token(Kind.BIT_OR, "|", pos++, line);
+		case '<':
+			// TODO : Check for <<
+			// TODO : Check for <=
+			return new Token(Kind.REL_LT, "<", pos++, line);
+		case '>':
+			// TODO : Check for >>
+			// TODO : Check for >=
+			return new Token(Kind.REL_GT, ">", pos++, line);
+		case '=':
+			// TODO : Check for ==
+			return new Token(Kind.ASSIGN, "(", pos++, line);
+		case '(':
+			return new Token(Kind.LPAREN, "(", pos++, line);
+		case ')':
+			return new Token(Kind.RPAREN, ")", pos++, line);
+		case '{':
+			return new Token(Kind.LCURLY, "{", pos++, line);
+		case '}':
+			return new Token(Kind.RCURLY, "}", pos++, line);
+		case '[':
+			return new Token(Kind.LSQUARE, "[", pos++, line);
+		case ']':
+			return new Token(Kind.RSQUARE, "]", pos++, line);
+		case ';':
+			return new Token(Kind.SEMI, ";", pos++, line);
+		case ':':
+			// TODO : Check for ::
+			return new Token(Kind.COLON, ":", pos++, line);
+		case ',':
+			return new Token(Kind.COMMA, ",", pos++, line);
+		case '.':
+			// TODO : Check for ..
+			// TODO : Check for ...
+			return new Token(Kind.DOT, ".", pos++, line);
+		default:
+			throw new LexicalException("Handle all the cases");
 		}
+	}
 
 }
