8aaf692a3c0ea5d2128d90b1a21e6807d447f2f4
Madhav Sodhani <madhav2302@gmail.com>
2019-09-13 02:08:47 -0400

    Handle White spaces and add tests

--- src/cop5556fa19/Scanner.java
+++ src/cop5556fa19/Scanner.java
@@ -15,12 +15,47 @@ package cop5556fa19;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import cop5556fa19.Token.Kind;
 
 public class Scanner {
 
+	@SuppressWarnings("serial")
+	private static final Map<String, Kind> KEYWORDS_TO_KIND = new HashMap<String, Kind>() {
+		{
+			put("and", Kind.KW_and);
+			put("break", Kind.KW_break);
+			put("do", Kind.KW_do);
+			put("else", Kind.KW_else);
+			put("elseif", Kind.KW_elseif);
+			put("end", Kind.KW_end);
+			put("false", Kind.KW_false);
+			put("for", Kind.KW_for);
+			put("function", Kind.KW_function);
+			put("goto", Kind.KW_goto);
+			put("if", Kind.KW_if);
+			put("in", Kind.KW_in);
+			put("local", Kind.KW_local);
+			put("nil", Kind.KW_nil);
+			put("not", Kind.KW_not);
+			put("or", Kind.KW_or);
+			put("repeat", Kind.KW_repeat);
+			put("return", Kind.KW_return);
+			put("then", Kind.KW_then);
+			put("true", Kind.KW_true);
+			put("until", Kind.KW_until);
+			put("while", Kind.KW_while);
+		}
+	};
+
+	private static final Set<String> KEYWORDS = KEYWORDS_TO_KIND.keySet();
+
 	private enum State {
-		START, AFTER_DIV, AFTER_XOR, AFTER_LT, AFTER_GT, AFTER_EQ, AFTER_COLON, AFTER_DOT, AFTER_DOTDOT, IN_DIGIT
+		START, AFTER_DIV, AFTER_XOR, AFTER_LT, AFTER_GT, AFTER_EQ, AFTER_COLON, AFTER_DOT, AFTER_DOTDOT, IN_DIGIT,
+		IN_IDEN, IN_STRING, AFTER_CR,;
 	}
 
 	Reader r;
@@ -63,6 +98,23 @@ public class Scanner {
 				line = currentLine;
 
 				switch (ch) {
+				// WHITE SPACES
+				case ' ':
+				case '\t':
+				case '\f':
+					getChar();
+					break;
+				// LINE TERMINATORS
+				case '\n':
+					currentPos = -1;
+					currentLine++;
+					getChar();
+					break;
+				case '\r':
+					state = State.AFTER_CR;
+					getChar();
+					break;
+				// OTHER TOKENS
 				case '+':
 					t = new Token(Kind.OP_PLUS, "+", pos, line);
 					getChar();
@@ -160,19 +212,48 @@ public class Scanner {
 					t = new Token(Kind.INTLIT, "0", pos, line);
 					getChar();
 					break;
+				// STRING LITERAL
+				case '"':
+				case '\'':
+					state = State.IN_STRING;
+					sb.append((char) ch);
+					getChar();
+					break;
+				// EOF
 				case -1:
 					t = new Token(Kind.EOF, "EOF", pos, line);
 					break;
-
 				default:
+					// INTEGER LITERAL
 					if (Character.isDigit((char) ch)) {
 						state = State.IN_DIGIT;
+						sb.append(Character.getNumericValue(ch));
+						getChar();
+						break;
+					}
+					// NAME and KEYWORD
+					if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+						state = State.IN_IDEN;
 						sb.append((char) ch);
 						getChar();
 						break;
 					}
 
+					throw new LexicalException(
+							"Illegal character: " + (char) ch + " at line: " + line + ", pos: " + pos);
 				}
+				break;
+			case AFTER_CR:
+				if (ch == '\n') {
+					currentPos = -1;
+					currentLine++;
+					getChar();
+				} else {
+					currentPos = 0;
+					currentLine++;
+				}
+
+				state = State.START;
 				break;
 			case AFTER_DIV:
 				if (ch == '/') {
@@ -258,9 +339,68 @@ public class Scanner {
 					t = new Token(Kind.INTLIT, result, pos, line);
 				}
 				break;
-			default:
-				// TODO
+			case IN_IDEN:
+				if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122) || (ch >= 48 && ch <= 57) || ch == 95
+						|| ch == 36) {
+					sb.append((char) ch);
+					getChar();
+				} else {
+					String result = sb.toString();
+					t = new Token(KEYWORDS.contains(result) ? KEYWORDS_TO_KIND.get(result) : Kind.NAME, result, pos,
+							line);
+				}
 				break;
+			case IN_STRING:
+				boolean isDoubleQuoteEnd = sb.charAt(0) == '"';
+
+				if (ch == -1) {
+					throw new LexicalException("Unexpected EOF for string literal at line: " + line + ",pos: " + pos);
+				}
+
+				if ((isDoubleQuoteEnd && (char) ch == '"') || (!isDoubleQuoteEnd && (char) ch == '\'')) {
+					sb.append((char) ch);
+					getChar();
+					t = new Token(Kind.STRINGLIT, sb.toString(), pos, line);
+					break;
+				}
+
+				if ((isDoubleQuoteEnd && (char) ch == '\'') || (!isDoubleQuoteEnd && (char) ch == '"')) {
+					throw new LexicalException("Illegal Character Found in string literal: " + (char) ch);
+				}
+
+				if (ch == '\\') {
+					getChar();
+					if (ch == -1) {
+						throw new LexicalException(
+								"Unexpected EOF for string literal at line: " + line + ",pos: " + pos);
+					}
+					switch (ch) {
+					case 'a':
+					case 'b':
+					case 'f':
+					case 'n':
+					case 'r':
+					case 't':
+					case 'v':
+					case '\\':
+					case '"':
+					case '\'':
+						sb.append('\\'); // Append the escape character we skipped
+						sb.append((char) ch);
+						getChar();
+						break;
+					default:
+						throw new LexicalException("Illegal Character Found in string literal \\");
+
+					}
+				} else {
+					sb.append((char) ch);
+					getChar();
+
+				}
+				break;
+			default:
+				throw new LexicalException("Unknown state found: " + state);
 			}
 		}
 
--- src/cop5556fa19/ScannerTest.java
+++ src/cop5556fa19/ScannerTest.java
@@ -36,7 +36,7 @@ class ScannerTest {
 			System.out.println(input.toString());
 		}
 	}
-	
+
 	// ***********************************
 	// Provided examples
 	// ***********************************
@@ -121,12 +121,12 @@ class ScannerTest {
 		assertEquals(t.kind, REL_EQEQ);
 		assertEquals(t.text, "==");
 	}
-	
+
 	@Test
 	void testWithEverything() throws Exception {
 		Reader r = new StringReader("+-*/%^#&~|<<>>//==00~=<=>1234567890=<>(22=){}[]::;:,.120.012.,,..,.");
 		Scanner s = new Scanner(r);
-		
+
 		int pos = 0;
 		assertToken(s, OP_PLUS, "+", pos++);
 		assertToken(s, OP_MINUS, "-", pos++);
@@ -149,7 +149,7 @@ class ScannerTest {
 
 		assertToken(s, REL_EQEQ, "==", pos++);
 		pos++;
-		
+
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "0", pos++);
 
@@ -160,19 +160,18 @@ class ScannerTest {
 		pos++;
 
 		assertToken(s, REL_GT, ">", pos++);
-		
+
 		assertToken(s, INTLIT, "1234567890", pos);
 		pos = pos + 10;
 
-		
 		assertToken(s, ASSIGN, "=", pos++);
 		assertToken(s, REL_LT, "<", pos++);
 		assertToken(s, REL_GT, ">", pos++);
 		assertToken(s, LPAREN, "(", pos++);
-		
+
 		assertToken(s, INTLIT, "22", pos++);
 		pos++;
-		
+
 		assertToken(s, ASSIGN, "=", pos++);
 		assertToken(s, RPAREN, ")", pos++);
 		assertToken(s, LCURLY, "{", pos++);
@@ -186,11 +185,11 @@ class ScannerTest {
 		assertToken(s, COLON, ":", pos++);
 		assertToken(s, COMMA, ",", pos++);
 		assertToken(s, DOT, ".", pos++);
-		
+
 		assertToken(s, INTLIT, "120", pos++);
 		pos++;
 		pos++;
-		
+
 		assertToken(s, DOT, ".", pos++);
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "12", pos++);
@@ -207,15 +206,151 @@ class ScannerTest {
 	}
 	
 	// ***********************************
+	// Test For WhiteSpaces
+	// ***********************************
+	
+	@Test
+	void handleAllWhiteSpaces() throws Exception {
+		Reader r = new StringReader(" \t\f\r\n\n\r");
+		Scanner s = new Scanner(r);
+		
+		assertToken(s, EOF, "EOF", 0, 3);
+	}
+	
+	@Test
+	void shouldSkipCRandLFasSingleNewLine() throws Exception {
+		Reader r = new StringReader("\r\n0\n");
+		Scanner s = new Scanner(r);
+		
+		assertToken(s, INTLIT, "0", 0, 1);
+		assertToken(s, EOF, "EOF", 0, 2);
+	}
+
+	// ***********************************
+	// Test For Name
+	// ***********************************
+	
+	@Test
+	void shouldThrowException_StartsWithUnknown() throws Exception {
+		Reader r = new StringReader("__");
+		Scanner s = new Scanner(r);
+		String errorMessage = "";
+		
+		try {
+			s.getNext();
+		} catch(LexicalException e) {
+			errorMessage = e.getMessage();
+		}
+		
+		assertTrue(errorMessage.contains("Illegal character"));
+		
+	}
+	
+	@Test
+	void handleIdentifiers() throws Exception {
+		Reader r = new StringReader("abcd012__$ 01234abcdef");
+		Scanner s = new Scanner(r);
+		int pos = 0;
+		
+		assertToken(s, NAME, "abcd012__$", pos);
+		pos += "abcd012__$".length() + 1;
+		
+		assertToken(s, INTLIT, "0", pos++);
+		assertToken(s, INTLIT, "1234", pos);
+		pos += 4;
+		
+		assertToken(s, NAME, "abcdef", pos);
+	}
+
+	// ***********************************
+	// Test For KeyWord
+	// ***********************************
+
+	@Test
+	void handleAllKeywords() throws Exception {
+		Reader r = new StringReader(
+				"and break do else elseif end false for function goto if in local nil not or repeat return then true until while");
+		Scanner s = new Scanner(r);
+
+		int pos = 0;
+
+		assertToken(s, KW_and, "and", pos);
+		pos += 3 + 1; // Each time we add 1 for the space
+
+		assertToken(s, KW_break, "break", pos);
+		pos += 5 + 1;
+
+		assertToken(s, KW_do, "do", pos);
+		pos += 2 + 1;
+
+		assertToken(s, KW_else, "else", pos);
+		pos += 4 + 1;
+
+		assertToken(s, KW_elseif, "elseif", pos);
+		pos += 6 + 1;
+
+		assertToken(s, KW_end, "end", pos);
+		pos += 3 + 1;
+
+		assertToken(s, KW_false, "false", pos);
+		pos += 5 + 1;
+
+		assertToken(s, KW_for, "for", pos);
+		pos += 3 + 1;
+
+		assertToken(s, KW_function, "function", pos);
+		pos += 8 + 1;
+
+		assertToken(s, KW_goto, "goto", pos);
+		pos += 4 + 1;
+
+		assertToken(s, KW_if, "if", pos);
+		pos += 2 + 1;
+
+		assertToken(s, KW_in, "in", pos);
+		pos += 2 + 1;
+
+		assertToken(s, KW_local, "local", pos);
+		pos += 5 + 1;
+
+		assertToken(s, KW_nil, "nil", pos);
+		pos += 3 + 1;
+
+		assertToken(s, KW_not, "not", pos);
+		pos += 3 + 1;
+
+		assertToken(s, KW_or, "or", pos);
+		pos += 2 + 1;
+
+		assertToken(s, KW_repeat, "repeat", pos);
+		pos += 6 + 1;
+
+		assertToken(s, KW_return, "return", pos);
+		pos += 6 + 1;
+
+		assertToken(s, KW_then, "then", pos);
+		pos += 4 + 1;
+
+		assertToken(s, KW_true, "true", pos);
+		pos += 4 + 1;
+
+		assertToken(s, KW_until, "until", pos);
+		pos += 5 + 1;
+
+		assertToken(s, KW_while, "while", pos);
+		pos += 5 + 1;
+	}
+
+	// ***********************************
 	// Test For Integer Literals
 	// ***********************************
 	@Test
 	void shouldThrowException_NumberOutOfRange() throws Exception {
 		String outOfRangeNumber = String.valueOf(Long.MAX_VALUE);
-		
+
 		Reader r = new StringReader(outOfRangeNumber);
 		Scanner s = new Scanner(r);
-		
+
 		String expectedMessage = "Number out of range: " + outOfRangeNumber;
 		String messageInException = "";
 		try {
@@ -223,46 +358,46 @@ class ScannerTest {
 		} catch (LexicalException e) {
 			messageInException = e.getMessage();
 		}
-		
+
 		assertEquals(messageInException, expectedMessage);
 	}
-	
+
 	@Test
 	void handleIntegerLiteral() throws Exception {
 		Reader r = new StringReader("012345678");
 		Scanner s = new Scanner(r);
-		
+
 		int pos = 0;
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "12345678", pos++);
 	}
-	
+
 	@Test
 	void handleNonZeroDigitIntegerLiteral() throws Exception {
 		Reader r = new StringReader("12345678");
 		Scanner s = new Scanner(r);
-		
+
 		int pos = 0;
 		assertToken(s, INTLIT, "12345678", pos++);
 	}
-	
+
 	@Test
 	void handleZeroDigitIntegerLiteral() throws Exception {
 		Reader r = new StringReader("00000");
 		Scanner s = new Scanner(r);
-		
+
 		int pos = 0;
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "0", pos++);
 		assertToken(s, INTLIT, "0", pos++);
-		
+
 	}
-	
+
 	// ***********************************
 	// Test For Other Tokens
-	// ***********************************	
+	// ***********************************
 
 	@Test
 	void handleOtherTokens() throws Exception {
@@ -332,11 +467,11 @@ class ScannerTest {
 	// ***********************************
 	// Common methods
 	// ***********************************
-	
+
 	void assertToken(Scanner s, Kind kind, String text, int pos) throws Exception {
 		assertToken(s, kind, text, pos, 0);
 	}
-	
+
 	void assertToken(Scanner s, Kind kind, String text, int pos, int line) throws Exception {
 		assertToken(s, new Token(kind, text, pos, line));
 	}
