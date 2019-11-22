869c8f9f42cf46f304c9657e12f8bd5e64d2d0f4
Madhav Sodhani <madhav2302@gmail.com>
2019-11-21 19:15:27 -0500

    Fix toNumber according to described in description

--- src/interpreter/built_ins/toNumber.java
+++ src/interpreter/built_ins/toNumber.java
@@ -1,13 +1,13 @@
 package interpreter.built_ins;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import interpreter.ASTVisitorAdapter.TypeException;
 import interpreter.JavaFunction;
 import interpreter.LuaInt;
 import interpreter.LuaString;
 import interpreter.LuaValue;
-import interpreter.ASTVisitorAdapter.TypeException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class toNumber extends JavaFunction {
 
@@ -15,12 +15,14 @@ public class toNumber extends JavaFunction {
 	public List<LuaValue> call(List<LuaValue> functionargs) throws TypeException {
 		LuaValue arg = functionargs.get(0);
 		List<LuaValue> retList = new ArrayList<>();
-		if (arg instanceof LuaInt) { 
+		if (arg instanceof LuaInt) {
 			retList.add(arg);
+			return retList;
 		}
 		if (arg instanceof LuaString) {
 			int num = Integer.parseInt(((LuaString)arg).value);
 			retList.add(new LuaInt(num));
+			return retList;
 		}
 		throw new TypeException("Cannot convert " + arg + "to a number");
 	}
